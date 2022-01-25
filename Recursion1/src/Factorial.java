14e4d77f1a9bbf7a21aee4cc26e8659a3d7ce952
Abhay 076 <yadavabhaycs@gmail.com>
2022-01-25 20:16:24 +0530

    Factorial

--- Recursion1/src/Factorial.java
+++ Recursion1/src/Factorial.java
@@ -1,9 +1,24 @@
 
+
+import java.util.Scanner;
+
 public class Factorial {
+	public static int fact(int n) {
+		if(n==1) {
+			return 1;
+		}
+		int smallOutput=fact(n-1);
+		int ans=smallOutput*n;
+		return  ans;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int ans=fact(n);
+        System.out.println(ans);
+        sc.close();
 	}
 
 }
