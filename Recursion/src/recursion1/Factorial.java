66629b5d10fa919bab85b0578eeb65c7c72ff1f0
Abhay 076 <yadavabhaycs@gmail.com>
2022-01-25 20:05:29 +0530

    factorial

--- Recursion/src/recursion1/Factorial.java
+++ Recursion/src/recursion1/Factorial.java
@@ -1,11 +1,24 @@
 package recursion1;
 
+import java.util.Scanner;
+
 public class Factorial {
+	public static int fact(int n) {
+		if(n==1) {
+			return 1;
+		}
+		int smallOutput=fact(n-1);
+		int ans=smallOutput*n;
+		return  ans;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("abc");
-
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int ans=fact(n);
+        System.out.println(ans);
+        sc.close();
 	}
 
 }
