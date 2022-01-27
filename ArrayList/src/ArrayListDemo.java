d8ca75bf90256875dca230c8faf64a370ee9b8e7
Abhay 076 <yadavabhaycs@gmail.com>
2022-01-27 11:54:32 +0530

    Arraylist Update

--- ArrayList/src/ArrayListDemo.java
+++ ArrayList/src/ArrayListDemo.java
@@ -9,6 +9,15 @@ public class ArrayListDemo {
 		list.add(20);
 		list.add(30);
 		list.add(1,80);
+//		list.remove(0);
+		list.set(0, 100);
+		for(int i=0;i<list.size();i++) {
+			System.out.println(list.get(i));
+		}
+		//enhanced for loop - for each loop
+		for(int i:list) {
+			System.out.println(i);
+		}
 		System.out.println(list.size());
 		System.out.println(list.get(0));
 
