442c477cafd856286d9f2207a9a0cd5935a542d2
Abhay 076 <yadavabhaycs@gmail.com>
2022-02-07 14:33:29 +0530

    Print Nodes At Depth K

---
+++ BinaryTree/src/PrintNodesAtDepthK.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class PrintNodesAtDepthK {
+	public static BinaryTreeNode<Integer> takeTreeInputBetter(boolean isRoot,int parentData,boolean isLeft){
+		if(isRoot) {
+		    System.out.println("enter the root");
+		 }
+		else {
+			if(isLeft) {
+				System.out.println("Enter left child of "+parentData);
+			}
+			else {
+				System.out.println("Enter right child of "+parentData);
+			}
+		}
+		Scanner sc= new Scanner(System.in);
+		int rootData=sc.nextInt();
+		if(rootData==-1) {
+			return null;
+		}
+		BinaryTreeNode<Integer> root= new BinaryTreeNode<Integer>(rootData);
+		BinaryTreeNode<Integer> leftChild=takeTreeInputBetter(false,rootData,true);
+		BinaryTreeNode<Integer> rightChild=takeTreeInputBetter(false,rootData,false);
+		root.left=leftChild;
+		root.right=rightChild;
+		return root;
+	}
+	public static void PrintTreeDetailed(BinaryTreeNode<Integer> root) {
+		if(root==null) {
+			return;
+		}
+        System.out.print(root.data+":");
+        if(root.left!=null) {
+        	System.out.print("L"+root.left.data+", ");
+        }
+        if(root.right!=null) {
+        	System.out.print("R"+root.right.data);
+        }
+        System.out.println();      
+		PrintTreeDetailed(root.left);
+		PrintTreeDetailed(root.right);
+	}
+	public static void printAtDepthK(BinaryTreeNode<Integer> root,int k) {
+		if(root==null) {
+			return;
+		}
+		if(k==0) {
+			System.out.println(root.data);
+			return;
+		}
+		printAtDepthK(root.left, k-1);
+		printAtDepthK(root.right, k-1);
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BinaryTreeNode<Integer> root=takeTreeInputBetter(true, 0, true);
+		printAtDepthK(root, 2);
+
+	}
+
+}
