ab3da2acd3b0e6d1016c6459998fdfe42707f9bb
Abhay 076 <yadavabhaycs@gmail.com>
2022-02-22 09:49:57 +0530

    Min Steps To 1 Using Memoization

--- Dynamic Programming/src/MinStepTo1.java
+++ Dynamic Programming/src/MinStepTo1.java
@@ -20,12 +20,56 @@ public class MinStepTo1 {
 	    }
 	    return Math.min(Output1, Math.min(Output2, Output3))+1;
 	}
+	
+	public static int countMinStepsToOneMemo(int n,int dp[]) {
+		if(n<=1) {
+			return 0;
+		}
+		if(n==2||n==3) {
+			return 1;
+		}
+		int ans1;
+		if(dp[n-1]==-1) {
+			ans1=countMinStepsToOneMemo(n-1, dp);
+			dp[n-1]=ans1;
+		}
+		else {
+			ans1=dp[n-1];
+		}
+		int ans2=Integer.MAX_VALUE;
+		if(n%2==0) {
+			if(dp[n/2]==-1) {
+				ans2=countMinStepsToOneMemo(n/2, dp);
+				dp[n/2]=ans2;
+			}
+			else {
+				ans2=dp[n/2];
+			}
+		}
+		int ans3=Integer.MAX_VALUE;
+		if(n%3==0) {
+			if(dp[n/3]==-1) {
+				ans3=countMinStepsToOneMemo(n/3, dp);
+				dp[n/3]=ans3;
+			}
+			else {
+				ans3=dp[n/3];
+			}
+		}
+		return Math.min(ans1, Math.min(ans2, ans3))+1;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int n=7;
-		int recursion=countMinStepsToOne(n);
-		System.out.println(recursion);
+//		int recursion=countMinStepsToOne(n);
+//		System.out.println(recursion);
+		int dp[]= new int[n+1];
+		for(int i=0;i<dp.length;i++) {
+			dp[i]=-1;
+		}
+		int ans=countMinStepsToOneMemo(n, dp);
+		System.out.println(ans);
 
 	}
 
