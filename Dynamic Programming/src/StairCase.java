e9636597864216733a9038b1a54d1780066334d6
Abhay 076 <yadavabhaycs@gmail.com>
2022-02-22 09:01:19 +0530

    Using Memoization

--- Dynamic Programming/src/StairCase.java
+++ Dynamic Programming/src/StairCase.java
@@ -17,12 +17,54 @@ public class StairCase {
 		long ans=x+y+z;
 		return ans;
 	}
+	
+	//memoization
+	public static long stairCaseMemo(int n, long dp[]) {
+		if(n==0||n==1) {
+			return 1;
+		}
+		else if(n==2) {
+			return 2;
+		}
+		else {
+			long ans1;
+			if(dp[n-1]==-1) {
+				ans1=stairCaseMemo(n-1, dp);
+				dp[n-1]=ans1;
+			}else {
+				ans1=dp[n-1];
+			}
+			long ans2;
+			if(dp[n-2]==-1) {
+				ans2=stairCaseMemo(n-2, dp);
+				dp[n-2]=ans2;
+			}
+			else {
+				ans2=dp[n-2];
+			}
+			long ans3;
+			if(dp[n-3]==-1) {
+				ans3=stairCaseMemo(n-3, dp);
+				dp[n-3]=ans3;
+			}
+			else {
+				ans3=dp[n-3];
+			}
+			long myAns=ans1+ans2+ans3;
+			return myAns;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n=10;
-		long ans=stairCase(n);
-		System.out.println(ans);
+		int n=4;
+//		long ans=stairCase(n);
+		long dp[]= new long[n+1];
+		for(int i=0;i<dp.length;i++) {
+			dp[i]=-1;
+		}
+		long ansMemoization=stairCaseMemo(n, dp);
+		System.out.println(ansMemoization);
 
 	}
 
