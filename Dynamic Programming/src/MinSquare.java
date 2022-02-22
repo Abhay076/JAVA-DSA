3ba27d7586e85036d3448ca2ddee9443a550a7d3
Abhay 076 <yadavabhaycs@gmail.com>
2022-02-22 14:40:52 +0530

    Min Square Iterative

--- Dynamic Programming/src/MinSquare.java
+++ Dynamic Programming/src/MinSquare.java
@@ -33,17 +33,36 @@ public class MinSquare {
         
         return minVal+1;
 	}
+	
+	public static int minSquareI(int n) {
+		int dp[]= new int[n+1];
+		dp[0]=0;
+		for(int i=1;i<=n;i++) {
+			int minAns=Integer.MAX_VALUE;
+			for(int j=1;j*j<=i;j++) {
+				int currAns=dp[i-(j*j)];
+				if(minAns>currAns) {
+					minAns=currAns;
+				}
+				
+			}
+			dp[i]=1+minAns;
+		}
+		return dp[n];
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int n=41;
 //		int ans=minSquar(n);
 //		System.out.println(ans);
-		int dp[]= new int[n+1];
-		for(int i=0;i<dp.length;i++) {
-			dp[i]=-1;
-		}
-		int ans=minCountHelper(n, dp);
+//		int dp[]= new int[n+1];
+//		for(int i=0;i<dp.length;i++) {
+//			dp[i]=-1;
+//		}
+//		int ans=minCountHelper(n, dp);
+//		System.out.println(ans);
+		int ans=minSquareI(n);
 		System.out.println(ans);
 
 	}
