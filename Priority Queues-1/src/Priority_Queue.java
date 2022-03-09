5b9441f9de05541b04cc3f8b2b88ce63b964c922
Abhay 076 <yadavabhaycs@gmail.com>
2022-03-09 13:00:57 +0530

    Implementation of Priority Queue

---
+++ Priority Queues-1/src/Priority_Queue.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Priority_Queue<T> {
+
+	ArrayList<Element<T>> heap;
+	
+	public Priority_Queue() {
+		heap= new ArrayList<>();
+	}
+	public void insert(T value,int priority) {
+		
+	}
+	public T getMin() {
+		
+	}
+	public T getRemove() {
+		
+	}
+	public int size() {
+		
+	}
+	public boolean isEmpty() {
+		
+	}
+}
