b457f8523215e93c6a044f63493234d0c7a8c66b
Abhay 076 <yadavabhaycs@gmail.com>
2022-03-14 07:17:46 +0530

    Trie Uses

---
+++ Tries/src/TrieUse.java
@@ -0,0 +1,13 @@
+
+public class TrieUse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Trie t= new Trie();
+		t.add("NOTE");
+		t.add("AND");
+		System.out.println(t.search("NOTE"));
+
+	}
+
+}
